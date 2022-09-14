f576f479100379f1d0dc0615359b772efeb7ff60
Aiden Storer <s201294@students.aie.edu.au>
2022-09-14 11:13:15 +0930

    Base Doot Gun

--- Doot/Assets/Scripts/DootGun.cs
+++ Doot/Assets/Scripts/DootGun.cs
@@ -7,20 +7,34 @@ public class DootGun : MonoBehaviour
     public LayerMask mask = -1;
     private float distance;
     public Vector3 p1;
+    CharacterController charCtrl;
     // Start is called before the first frame update
     void Start()
     {
-        
+        charCtrl = GetComponentInParent<CharacterController>();
+        Debug.Log(charCtrl.name);
     }
 
     // Update is called once per frame
     void Update()
     {
-/*        Ray ray;
-        if (Physics.SphereCast(p1, 15, transform.forward, ray, 10))
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            Debug.Log("Hit");
-        }*/
+            Doot();
+        }
+    }
 
+    void Doot()
+    {
+        RaycastHit hit;
+
+        Vector3 p1 = transform.position + charCtrl.center;
+        if (Physics.SphereCast(p1, charCtrl.height / 2, transform.forward, out hit, 20))
+        {
+            if (hit.collider.tag == "Enemy")
+            {
+                Debug.Log("Push");
+            }
+        }
     }
 }
